3400a3657942c80e5dbbbf624d4b2173ea54af57
Mathieu Bourgeois <mathieuuu@gmail.com>
2018-05-31 10:21:44 +0200

    minor

--- src/rg/RgChecker.java
+++ src/rg/RgChecker.java
@@ -8,41 +8,37 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.Date;
+import java.time.LocalDateTime;
 
-public class Main {
+public class RgChecker {
 
     public static void main(String[] args) throws UnirestException, IOException, InterruptedException {
-
 
         while (true) {
             System.out.println("Wait...");
             Thread.sleep(5000);
-            Date d = new Date();
-            System.out.println(d.toString()+" Check");
+            System.out.println(LocalDateTime.now().toString()+" Check");
             check();
         }
     }
 
 
     private static void check() throws UnirestException, IOException {
-        HttpResponse<JsonNode> req = Unirest.get("https://tickets.rolandgarros.com/api/tunnel/products/day/20180531/court/SL?_=1527706370007")
-                .asJson();
+        HttpResponse<JsonNode> res = Unirest.get("https://tickets.rolandgarros.com/api/tunnel/products/day/20180531/court/SL?_=1527706370007").asJson();
 
-        JsonNode json = req.getBody();
-
+        JsonNode json = res.getBody();
         JSONArray arr = json.getArray();
 
         for (int i = 0; i < arr.length(); i++) {
             JSONObject o = arr.getJSONObject(i);
-            int p = o.getInt("priceAmount");
-            int s = o.getInt("seats");
+            int price = o.getInt("priceAmount");
+            int seats = o.getInt("seats");
 
-            if (s > 0 && p < 90) {
+            if (seats > 0 && price < 90) {
                 Runtime.getRuntime().exec(new String[] {
                         "osascript",
                         "-e",
-                        "display notification \""+p+"\" with title \"Title\" subtitle \"Subtitle\" sound name \"Basso\"" });
+                        "display notification \""+seats+" places à "+price+"€\" with title \"Places dispos !\" subtitle \"\" sound name \"Basso\"" });
             }
         }
     }
